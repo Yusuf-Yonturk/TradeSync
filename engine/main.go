71a32852b09ffcaf6ce6bebcdf393759395b9ffc
Yusuf Yonturk <yusufyonturk.kisisel@gmail.com>
2026-05-04 17:13:08 +0000

    created orderbook and test-order

--- engine/main.go
+++ engine/main.go
@@ -5,12 +5,13 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"tradesync/engine/orderbook"
 )
 
 func main() {
 	eR := http.NewServeMux()
 
-	eR.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	eR.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(map[string]string{
@@ -19,6 +20,24 @@ func main() {
 			"time":    time.Now().UTC().Format(time.RFC3339),
 		})
 	})
+
+	eR.HandleFunc("GET /test-order", func(w http.ResponseWriter, r *http.Request) {
+		order := orderbook.Order{
+			ID:        "ord-1",
+			UserID:    "user-1",
+			Side:      orderbook.Buy,
+			Price:     10500,
+			Quantity:  10,
+			FilledQty: 3,
+			Status:    orderbook.Open,
+			Timestamp: time.Now(),
+		}
+		json.NewEncoder(w).Encode(map[string]any{
+			"id":        order.ID,
+			"remaining": order.RemainingQty(),
+			"is_filled": order.IsFilled(),
+		})
+	})	
 	log.Printf("engine basliyor")
 	err := http.ListenAndServe(":8081", eR)
 	if err != nil {
---
+++ engine/orderbook/order.go
@@ -0,0 +1,40 @@
+package orderbook
+
+import "time"
+
+type Side string
+
+const (
+	Buy  Side = "BUY"
+	Sell Side = "SELL"
+)
+
+type OrderStatus string
+
+const (
+	Open           OrderStatus = "OPEN"
+	PartiallFilled OrderStatus = "PARTIALLY_FILLED"
+	Filled         OrderStatus = "FILLED"
+	Cancelled      OrderStatus = "CANCELLED"
+)
+
+type Order struct {
+	ID        string
+	UserID    string
+	Side      Side
+	Price     int64
+	Quantity  int64
+	FilledQty int64
+	Status    OrderStatus
+	Timestamp time.Time
+
+	HeapIndex int
+}
+
+func (o *Order) RemainingQty() int64 {
+	return o.Quantity - o.FilledQty
+}
+
+func (o *Order) IsFilled() bool {
+	return o.FilledQty >= o.Quantity
+}
